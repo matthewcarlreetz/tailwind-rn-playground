7a14da154183d3ec9f0d110629a471d804ebdde4
Matthew Reetz <matt@headway.io>
2020-08-21 08:57:51 -0500

    Create expo app and configure tailwind rn.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, View, Text } from 'react-native';
+import tailwind from 'tailwind-rn';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => (
+  <SafeAreaView style={tailwind('h-full')}>
+    <View style={tailwind('pt-12 items-center')}>
+      <View style={tailwind('bg-blue-200 p-5 rounded-full')}>
+        <Text style={tailwind('text-blue-800 font-semibold')}>Hello Tailwind</Text>
+      </View>
     </View>
-  );
-}
+  </SafeAreaView>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
